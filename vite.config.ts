0d7de012cd46e67c6fa31f36941d19fe89cdd720
FIL708 <filip.zebrowski4013@gmail.com>
2024-08-23 14:15:32 +0200

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import checker from 'vite-plugin-checker'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import * as path from 'path'
 
 export default defineConfig({
-    plugins: [react(), checker({ typescript: true })],
+    plugins: [react(), checker({ typescript: true }), tsconfigPaths()],
+    resolve: {
+        alias: { '@': path.resolve(__dirname, './src') },
+    },
     test: {
         environment: 'jsdom',
         setupFiles: ['./vitest.setup.ts'],
