ec84b1e2ac4dd9b1f563bd2b60777862ade27f47
FIL708 <filip.zebrowski4013@gmail.com>
2024-08-23 12:06:23 +0200

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import checker from 'vite-plugin-checker'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), checker({ typescript: true })],
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['./vitest.setup.ts'],
+    },
 })
